48d627004762a1b4623b3e5db1957320740901bb
prodigy234 <k.gbenga@gmail.com>
2023-12-11 11:05:01 +0100

    Changes

--- lib/features/authentication/auth_screens/login_screen.dart
+++ lib/features/authentication/auth_screens/login_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:fashion_ecommerce_app/features/authentication/auth_screens/sign_up_screen.dart';
 import 'package:fashion_ecommerce_app/features/screens/category_screen.dart';
+import 'package:fashion_ecommerce_app/features/services/auth_service.dart';
 import 'package:fashion_ecommerce_app/features/widgets/auth_input_text_field_widget.dart';
 import 'package:fashion_ecommerce_app/features/widgets/long_button_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -25,47 +26,47 @@ class _LoginScreenState extends State<LoginScreen> {
     passwordController.dispose();
   }
 
-  FirebaseAuth firebaseauth = FirebaseAuth.instance;
+  // FirebaseAuth firebaseauth = FirebaseAuth.instance;
 
-  login() async {
-    User? currentUser;
+  // login() async {
+  //   User? currentUser;
 
-    await firebaseauth
-        .signInWithEmailAndPassword(
-            email: emailController.text.trim(),
-            password: passwordController.text.trim())
-        .then((auth) {
-      currentUser = auth.user;
-    }).catchError((error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.red,
-          content: Text(error.toString()),
-        ),
-      );
-    });
-    if (currentUser != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          backgroundColor: Colors.green,
-          content: Text('Login successful'),
-        ),
-      );
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const BottomnavScreen(),
-        ),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          backgroundColor: Colors.red,
-          content: Text('Not logged in'),
-        ),
-      );
-    }
-  }
+  // await firebaseauth
+  //     .signInWithEmailAndPassword(
+  //         email: emailController.text.trim(),
+  //         password: passwordController.text.trim())
+  //     .then((auth) {
+  //   currentUser = auth.user;
+  // }).catchError((error) {
+  //   ScaffoldMessenger.of(context).showSnackBar(
+  //     SnackBar(
+  //       backgroundColor: Colors.red,
+  //       content: Text(error.toString()),
+  //     ),
+  //   );
+  // });
+  // if (currentUser != null) {
+  //   ScaffoldMessenger.of(context).showSnackBar(
+  //     const SnackBar(
+  //       backgroundColor: Colors.green,
+  //       content: Text('Login successful'),
+  //     ),
+  //   );
+  //     Navigator.push(
+  //       context,
+  //       MaterialPageRoute(
+  //         builder: (context) => const BottomnavScreen(),
+  //       ),
+  //     );
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(
+  //         backgroundColor: Colors.red,
+  //         content: Text('Not logged in'),
+  //       ),
+  //     );
+  //   }
+  // }
 
   bool isObscure = true;
 
@@ -77,163 +78,175 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 120,
-                    width: 120,
-                    child: Image.asset(
-                      'assets/images/Fashion Nova.png',
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30),
-              Text(
-                'Welcome',
-                style: GoogleFonts.poppins(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              Text(
-                'Pls login or Signup to continue',
-                style: GoogleFonts.poppins(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              AuthInputTextField(
-                controller: emailController,
-                obscureText: false,
-                labelText: 'Email',
-                validate: (email) {
-                  if (email == null || email.isEmpty) {
-                    return 'Please enter your email address';
-                  } else if (!email.contains('@') || !email.contains('')) {
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 30),
-              AuthInputTextField(
-                controller: passwordController,
-                obscureText: isObscure,
-                suffixIcon: InkWell(
-                  onTap: () {
-                    setState(() {
-                      isObscure = !isObscure;
-                    });
-                  },
-                  child: isObscure == true
-                      ? const Icon(Icons.visibility_off_outlined)
-                      : const Icon(Icons.visibility_outlined),
-                ),
-                labelText: 'Password',
-                validate: (password) {
-                  if (password == null || password.isEmpty) {
-                    return 'Please enter your password';
-                  } else if (password.length < 6) {
-                    return 'Password must be a minimum of 6 characters';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 30),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    isLoading = true;
-                  });
-                  if (_formKey.currentState!.validate()) {
-                    login();
-                    setState(() {
-                      isLoading = false;
-                    });
-                  }
-                },
-                child: LongButtonContainer(
-                  buttonName: isLoading ? 'Loading...' : 'Login',
-                  buttonColor: Colors.black,
-                  buttonTextColor: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Center(
-                child: Row(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text("Don't have an account?"),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SignupScreen()));
-                      },
-                      child: Text(
-                        "Sign up",
-                        style: GoogleFonts.poppins(color: Colors.blue),
+                    SizedBox(
+                      height: 120,
+                      width: 120,
+                      child: Image.asset(
+                        'assets/images/Fashion Nova.png',
                       ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: 1,
-                    width: 150,
-                    decoration: const BoxDecoration(
-                      color: Colors.grey,
-                    ),
+                const SizedBox(height: 30),
+                Text(
+                  'Welcome',
+                  style: GoogleFonts.poppins(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const Text('Or'),
-                  Container(
-                    height: 1,
-                    width: 150,
-                    decoration: const BoxDecoration(
-                      color: Colors.grey,
-                    ),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  'Pls login or Signup to continue',
+                  style: GoogleFonts.poppins(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              const LongButtonContainer(
-                buttonName: 'Continue with Facebook',
-                icon: Icons.facebook_outlined,
-                buttonColor: Colors.blue,
-                buttonTextColor: Colors.white,
-              ),
-              const SizedBox(height: 10),
-              const LongButtonContainer(
-                buttonName: 'Continue with Google',
-                buttonColor: Colors.white,
-                buttonTextColor: Colors.black,
-              ),
-              const SizedBox(height: 20),
-              const LongButtonContainer(
-                buttonName: 'Continue with Apple',
-                buttonColor: Colors.transparent,
-                buttonTextColor: Colors.black,
-              ),
-            ],
+                ),
+                const SizedBox(height: 30),
+                AuthInputTextField(
+                  controller: emailController,
+                  obscureText: false,
+                  labelText: 'Email',
+                  validate: (email) {
+                    if (email == null || email.isEmpty) {
+                      return 'Please enter your email address';
+                    } else if (!email.contains('@') || !email.contains('')) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 30),
+                AuthInputTextField(
+                  controller: passwordController,
+                  obscureText: isObscure,
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    child: isObscure == true
+                        ? const Icon(Icons.visibility_off_outlined)
+                        : const Icon(Icons.visibility_outlined),
+                  ),
+                  labelText: 'Password',
+                  validate: (password) {
+                    if (password == null || password.isEmpty) {
+                      return 'Please enter your password';
+                    } else if (password.length < 6) {
+                      return 'Password must be a minimum of 6 characters';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 30),
+                GestureDetector(
+                  onTap: () {
+                    // if (_formKey.currentState!.validate()) {
+                    // setState(() {
+                    //   isLoading = true;
+                    // });
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        isLoading = true;
+                      });
+                      AuthService()
+                          .login(
+                        email: emailController.text.trim(),
+                        password: passwordController.text.trim(),
+                      )
+                          .then((_) {
+                        setState(() {
+                          isLoading = false;
+                        });
+                      });
+                    }
+                    // login();
+                  },
+                  child: LongButtonContainer(
+                    buttonName: isLoading == true ? 'Loading...' : 'Login',
+                    buttonColor: Colors.black,
+                    buttonTextColor: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account?"),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignupScreen()));
+                        },
+                        child: Text(
+                          "Sign up",
+                          style: GoogleFonts.poppins(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 1,
+                      width: 150,
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const Text('Or'),
+                    Container(
+                      height: 1,
+                      width: 150,
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const LongButtonContainer(
+                  buttonName: 'Continue with Facebook',
+                  icon: Icons.facebook_outlined,
+                  buttonColor: Colors.blue,
+                  buttonTextColor: Colors.white,
+                ),
+                const SizedBox(height: 10),
+                const LongButtonContainer(
+                  buttonName: 'Continue with Google',
+                  buttonColor: Colors.white,
+                  buttonTextColor: Colors.black,
+                ),
+                const SizedBox(height: 20),
+                const LongButtonContainer(
+                  buttonName: 'Continue with Apple',
+                  buttonColor: Colors.transparent,
+                  buttonTextColor: Colors.black,
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
---
+++ lib/features/services/auth_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  Future<void> login({required String email, required String password}) async {
+    String url = 'https://reqres.in/api/login';
+
+    Map<String, dynamic> body = {
+      "email": email,
+      "password": password,
+    };
+
+    debugPrint(body.toString());
+
+    var response = await http.post(Uri.parse(url), body: body);
+
+    debugPrint(body.toString());
+    debugPrint(response.statusCode.toString());
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return data;
+    } else {
+      debugPrint('Error');
+    }
+  }
+}
